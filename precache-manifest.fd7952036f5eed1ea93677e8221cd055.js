8004e62b211d3c218c116aad623a9eded60e90b3
anzana000 <anzana000@gmail.com>
2020-09-08 21:23:24 +0545

    Updates

---
+++ precache-manifest.fd7952036f5eed1ea93677e8221cd055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9871aa89b6e5a8f6c8c9e111c7150975",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ee4e5e153d0b1976083c",
+    "url": "/monsters-rolodex/static/css/main.7cec33b6.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/monsters-rolodex/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4e5e153d0b1976083c",
+    "url": "/monsters-rolodex/static/js/main.83bcf93d.chunk.js"
+  },
+  {
+    "revision": "9928de3c8707531b6819",
+    "url": "/monsters-rolodex/static/js/runtime-main.6f6d5f45.js"
+  }
+]);
